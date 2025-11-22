e7002c1be3a48f2e217c135d22c297f593f8096e
GNADOU ANGE WILLIAM GOLE <167898523+Williamange@users.noreply.github.com>
2025-11-22 02:27:48 -0500

    Update livres.java

--- livres.java
+++ livres.java
@@ -1,2 +1,10 @@
 public class livres {
+    public int idLivre;
+    public String nomLivre;
+    public livres(int idLivre, String nomLivre) {
+        this.idLivre = idLivre;
+        this.nomLivre = nomLivre;
+    }
+    public String getNomLivre() { return nomLivre; }
+    public int getIdLivre() { return idLivre; }
 }
